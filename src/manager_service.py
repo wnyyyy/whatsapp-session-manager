99e7613b5588209767cd658145f74f993262cfe7
wny <viniberizzi@gmail.com>
2023-12-04 12:30:05 -0300

    now uses thread instead of multiprocessing for easier resource sharing

--- src/manager_service.py
+++ src/manager_service.py
@@ -1,16 +1,16 @@
-from asyncio import Lock
-import asyncio
 import os
 import util
 import consts
 import json
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from session import Session
 
 class ManagerService:
     def __init__(self):
         self.sessions = []
+        self.service = Service(ChromeDriverManager().install())
         self._load_sessions()    
-        self.check_sessions = asyncio.create_task(self._check_sessions())    
         
     def create_session(self, session_name):
         session_path = util.get_session_path(session_name)
@@ -23,15 +23,9 @@ class ManagerService:
         session_names.append(session_name)
         with open(config_path, 'w') as file:
             json.dump(session_names, file)
-        session = Session(session_name)
+        session = Session(session_name, self.service)
         self.sessions.append(session)
         return session
-    
-    async def _check_sessions(self):
-        while True:
-            await asyncio.sleep(1)
-            for session in self.sessions:
-                session.check_responses()
     
     def _load_sessions(self):
         config_path = consts.WORK_DIR + '/config.json'
@@ -43,6 +37,6 @@ class ManagerService:
             with open(config_path, 'r') as file:
                 session_names = json.load(file)
         for session_name in session_names:
-            session = Session(session_name)
+            session = Session(session_name, self.service)
             self.sessions.append(session)
         
--- src/session.py
+++ src/session.py
@@ -1,59 +1,52 @@
+from threading import Lock, Thread
 import time
 import util
-from multiprocessing import Process, Queue
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 class Session:
-    def __init__(self, name: str):
+    def __init__(self, name: str, service: Service):
         self.name = name
         self.running = False
         self.logged_in = False
-        self.process = None
-        self.command_queue = Queue()
-        self.response_queue = Queue()
+        self.thread = None
+        self.service = service
+        self.lock = Lock()
         
     def quit(self):
-        if self.running:
-            self.send_command('QUIT')
-            if self.process is not None:
-                self.process.join()
-            self.process = None
-            self.running = False
-            self.logged_in = False
-        
-    def send_command(self, command):
-        if self.running:
-            self.command_queue.put(command)
+        try:
+            self.lock.acquire()
+            if self.running:
+                if self.thread is not None:
+                    self.thread.join()
+                self.thread = None
+                self.running = False
+                self.logged_in = False
+        finally:
+            self.lock.release()
         
     def run(self):
-        if self.running:
-            return
-        process = Process(target=self._run, args=(self.command_queue, self.response_queue, self.name))
-        process.start()
-        self.running = True
-        self.process = process
+        try:
+            self.lock.acquire()
+            if self.running:
+                return
+            self.thread = Thread(target=self._run)
+            self.thread.start()
+        finally:
+            self.lock.release()
         
-    def check_responses(self):
-        if self.running:
-            while not self.response_queue.empty():
-                response = self.response_queue.get()
-                if response == "LOGGED_IN":
-                    self.logged_in = True
-        
-    @staticmethod
-    def _run(command_queue: Queue, response_queue: Queue, name: str):
-        session_path = util.get_session_path(name)
+    def _run(self):
+        session_path = util.get_session_path(self.name)
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument(f'--user-data-dir={session_path}')
                
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver = webdriver.Chrome(service=self.service, options=chrome_options)
         driver.get('https://web.whatsapp.com')
         
-        while True:
-            command = command_queue.get()
-            if command == 'QUIT':
-                driver.quit()
-                break
+        try:
+            self.lock.acquire()
+            self.running = True
+        finally:
+            self.lock.release()
+        
+        time.sleep(9999)
