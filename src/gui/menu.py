dd010f7cacf7e2e26e6eff42d2df5f91adaad9d6
wny <viniberizzi@gmail.com>
2023-12-05 23:19:55 -0300

    made table functional

--- src/gui/menu.py
+++ src/gui/menu.py
@@ -1,26 +1,25 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLineEdit, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLineEdit, QTableWidget, QTableWidgetItem, QHeaderView, QHBoxLayout
+from PyQt5.QtCore import Qt, QTimer
+
+from gui.execute_button import ExecuteButton
 
 class Menu(QMainWindow):
     def __init__(self, manager):
         super().__init__()
         self.manager = manager
         self.initUI()
+        self.start_polling()
 
     def initUI(self):
         self.setWindowTitle('Session Manager')
-        self.setGeometry(100, 100, 600, 400)
+        self.setGeometry(625, 325, 600, 400)
+        self.resize(600, 400)
         
         layout = QVBoxLayout()
-
-        self.table = QTableWidget(len(self.manager.sessions), 3)
-        self.table.setHorizontalHeaderLabels(["Session", "Running", "Logged In"])
-        for i, session in enumerate(self.manager.sessions):
-            self.table.setItem(i, 0, QTableWidgetItem(session.name))
-            self.table.setItem(i, 1, QTableWidgetItem(str(session.running)))
-            self.table.setItem(i, 2, QTableWidgetItem(str(session.logged_in)))
+        self.create_table()        
         layout.addWidget(self.table)
-
-        self.nameInput = QLineEdit(self)
+        
+        self.nameInput = QLineEdit(self)        
         layout.addWidget(self.nameInput)
 
         createButton = QPushButton('Create Session', self)
@@ -29,8 +28,33 @@ class Menu(QMainWindow):
 
         container = QWidget()
         container.setLayout(layout)
-
         self.setCentralWidget(container)
+        
+    def start_polling(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_table)
+        self.timer.start(100)
+        
+    def create_table(self):
+        self.table = QTableWidget(len(self.manager.sessions), 4)
+        self.table.setHorizontalHeaderLabels(['Session', 'Running', 'Logged In', None])
+        self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
+        for i, session in enumerate(self.manager.sessions):
+            self.table.setItem(i, 0, QTableWidgetItem(session.name))
+            self.table.setItem(i, 1, QTableWidgetItem(str(session.running)))
+            self.table.setItem(i, 2, QTableWidgetItem(str(session.logged_in)))
+            button = ExecuteButton(session)
+            btn_layout = QHBoxLayout()            
+            btn_layout.addWidget(button.btn)
+            btn_layout.setAlignment(Qt.AlignCenter)
+            btn_widget = QWidget()
+            btn_widget.setLayout(btn_layout)
+            self.table.setCellWidget(i, 3, btn_widget)
+        
+    def update_table(self):
+        for i, session in enumerate(self.manager.sessions):
+            self.table.setItem(i, 1, QTableWidgetItem(str(session.running)))
+            self.table.setItem(i, 2, QTableWidgetItem(str(session.logged_in)))        
 
     def createSession(self):
         session_name = self.nameInput.text()
