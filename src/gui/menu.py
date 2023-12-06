5c0984f8c4b567b2a5c510f021cb435903545264
wny <viniberizzi@gmail.com>
2023-12-06 02:07:39 -0300

    fix table refresh on creation

--- src/gui/menu.py
+++ src/gui/menu.py
@@ -13,10 +13,11 @@ class Menu(QMainWindow):
     def initUI(self):
         self.setWindowTitle('Session Manager')
         self.setGeometry(625, 325, 600, 400)
-        self.resize(600, 400)
+        self.resize(650, 400)
         
         layout = QVBoxLayout()
-        self.create_table()        
+        self.table = QTableWidget(0, 5)
+        self.create_table()
         layout.addWidget(self.table)
         
         self.nameInput = QLineEdit(self)        
@@ -36,28 +37,48 @@ class Menu(QMainWindow):
         self.timer.start(100)
         
     def create_table(self):
-        self.table = QTableWidget(len(self.manager.sessions), 4)
-        self.table.setHorizontalHeaderLabels(['Session', 'Running', 'Logged In', None])
+        self.table.setRowCount(0)
+        self.table.setRowCount(len(self.manager.sessions))
+        self.table.setHorizontalHeaderLabels(['Session', 'Number', 'Running', 'Logged In', None])
         self.table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
-        for i, session in enumerate(self.manager.sessions):
+        for i, session in enumerate(self.manager.sessions):            
             self.table.setItem(i, 0, QTableWidgetItem(session.name))
-            self.table.setItem(i, 1, QTableWidgetItem(str(session.running)))
-            self.table.setItem(i, 2, QTableWidgetItem(str(session.logged_in)))
+            
+            number_item = QTableWidgetItem(session.number)
+            number_item.setTextAlignment(Qt.AlignCenter)            
+            self.table.setItem(i, 1, number_item)
+            self.table.setColumnWidth(1, 150)
+            
+            running_item = QTableWidgetItem(str(session.running))
+            running_item.setTextAlignment(Qt.AlignCenter)            
+            self.table.setItem(i, 2, running_item)
+            
+            logged_in_item = QTableWidgetItem(str(session.logged_in))
+            logged_in_item.setTextAlignment(Qt.AlignCenter)
+            self.table.setItem(i, 3, logged_in_item)
+            
             button = ExecuteButton(session)
             btn_layout = QHBoxLayout()            
             btn_layout.addWidget(button.btn)
             btn_layout.setAlignment(Qt.AlignCenter)
             btn_widget = QWidget()
             btn_widget.setLayout(btn_layout)
-            self.table.setCellWidget(i, 3, btn_widget)
-            self.table.setRowHeight(i, button.btn.sizeHint().height()*2)
+            self.table.setCellWidget(i, 4, btn_widget)
+            self.table.setRowHeight(i, button.btn.sizeHint().height()*2)            
         
     def update_table(self):
         for i, session in enumerate(self.manager.sessions):
-            self.table.setItem(i, 1, QTableWidgetItem(str(session.running)))
-            self.table.setItem(i, 2, QTableWidgetItem(str(session.logged_in)))        
+            running_item = QTableWidgetItem(str(session.running))
+            running_item.setTextAlignment(Qt.AlignCenter)            
+            self.table.setItem(i, 2, running_item)
+            
+            logged_in_item = QTableWidgetItem(str(session.logged_in))
+            logged_in_item.setTextAlignment(Qt.AlignCenter)
+            self.table.setItem(i, 3, logged_in_item)     
 
     def createSession(self):
         session_name = self.nameInput.text()
-        if session_name != '' and session_name not in self.manager.sessions and session_name.isalnum():
-            self.manager.create_session(session_name)
+        if session_name != '' and all(session_name != s.name for s in self.manager.sessions) and session_name.isalnum():
+            self.manager.create_session(session_name, '00000000000')
+        self.create_table()
+        self.nameInput.clear()
