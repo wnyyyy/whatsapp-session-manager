0921bd57d6555dbca5010c28cc11cca4156fcb3d
wny <viniberizzi@gmail.com>
2023-12-07 17:44:47 -0300

    add constant for ui update

--- src/common/consts.py
+++ src/common/consts.py
@@ -5,4 +5,6 @@ PAGE_LOAD_TIMEOUT_SECONDS = 20
 DEFAULT_TIMEOUT_SECONDS = 5
 UI_INTERACTION_DELAY = 0.3
 
+UI_REFRESH_RATE = 500
+
 CSV_HEADER = 'Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,Phone 1 - Type,Phone 1 - Value\n'
