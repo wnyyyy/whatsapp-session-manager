afc70da687e197e8c2d4a83994b8c0771e9ef521
wny <viniberizzi@gmail.com>
2023-12-07 15:35:52 -0300

    begin group creation function

--- src/common/consts.py
+++ src/common/consts.py
@@ -2,5 +2,7 @@ import os
 
 WORK_DIR = f'{os.getenv('LOCALAPPDATA')}/WhatsApp Session Manager'
 PAGE_LOAD_TIMEOUT_SECONDS = 20
+DEFAULT_TIMEOUT_SECONDS = 5
+UI_INTERACTION_DELAY = 0.3
 
 CSV_HEADER = 'Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,Phone 1 - Type,Phone 1 - Value\n'
--- src/manager/session.py
+++ src/manager/session.py
@@ -1,6 +1,8 @@
 from threading import Lock, Thread
+import time
 import common.consts as consts
 import common.util as util
+from common.enum import WhatsAppContext
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import ChromeOptions, Chrome
 from selenium.webdriver.common.by import By
@@ -15,6 +17,7 @@ class Session:
         self.number = number
         self.running = False
         self.logged_in = None
+        self.context = None
         self.thread = None
         self.driver = None
         self.service = service
@@ -72,51 +75,91 @@ class Session:
                 EC.presence_of_element_located((By.CSS_SELECTOR, '.landing-title'))
             )
             self.logged_in = False
+            self.context = WhatsAppContext.LANDING_PAGE
         except TimeoutException:
             try:
-                WebDriverWait(self.driver, consts.PAGE_LOAD_TIMEOUT_SECONDS).until(
+                WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="profile photo"]'))
                 )
                 self.logged_in = True
+                self.context = WhatsAppContext.HOME
             except TimeoutException:
                 self.logged_in = None
-        self.get_contact('+55 62 8160-0359')
                 
-    def get_contact(self, contact):
+    def begin_group_creation(self):
         try:
             self.lock.acquire()
-            if not self.running:
+            if self.context != WhatsAppContext.HOME:
                 return None
+            time.sleep(consts.UI_INTERACTION_DELAY)
+            menu = WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[data-icon="menu"]')))
+            menu.click()
+                            
+            time.sleep(consts.UI_INTERACTION_DELAY)
+            new_group_button = WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="New group"]')))
+            new_group_button.click()
+            self.context = WhatsAppContext.GROUP_MEMBERS_SELECT       
+        except TimeoutException:
+            return None            
+        finally:
+            self.lock.release()
+            
+    def add_group_member(self, contact):
+        try:
+            self.lock.acquire()
+            if self.context != WhatsAppContext.GROUP_MEMBERS_SELECT:
+                return None
+            time.sleep(consts.UI_INTERACTION_DELAY)
+            search_box = WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="text"].copyable-text.selectable-text')))
+            
+            time.sleep(consts.UI_INTERACTION_DELAY)
+            search_box.send_keys(contact)
+            
+            time.sleep(consts.UI_INTERACTION_DELAY)
             try:
-                search_box = WebDriverWait(self.driver, consts.PAGE_LOAD_TIMEOUT_SECONDS).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '[title="Search input textbox"]'))
-                )
-                search_box.click()
-                search_box.send_keys(contact)
-            except TimeoutException:
+                results_div = WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, '[data-tab="4"]')))
+            except:
+                return False
+            results_div = results_div.find_elements(By.XPATH, "././*")
+            if len(results_div) > 1:
+                return False
+            
+            time.sleep(consts.UI_INTERACTION_DELAY)
+            results_div[0].click()
+            
+        except TimeoutException:
+            return None
+        finally:
+            self.lock.release()
+                
+    def contact_check(self, contact):
+        try:
+            self.lock.acquire()
+            if self.context != WhatsAppContext.HOME:
                 return None
             
-            try:
-                side_pane = WebDriverWait(self.driver, consts.PAGE_LOAD_TIMEOUT_SECONDS).until(
-                    EC.presence_of_element_located((By.ID, 'pane-side'))
-                )
-            except:
-                return None
-            
-            try:
-                WebDriverWait(self.driver, consts.PAGE_LOAD_TIMEOUT_SECONDS).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="Search results."]'))
-                )
-                search_results = side_pane.find_element(By.CSS_SELECTOR, '[aria-label="Search results."]')
-                if search_results is None:
-                    return False
-                search_results = search_results.find_elements(By.XPATH, "./*")
-                if len(search_results) >= 2:
-                    print("MAIS DE UM CONTATO ENCONTRADO!!!!111!")
-                    input()
-                contact = search_results[1]
-                return True
-            except:
-                return None
+            search_box = WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[title="Search input textbox"]')))
+            search_box.send_keys(contact)
+
+            side_pane = WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                EC.presence_of_element_located((By.ID, 'pane-side')))
+
+            WebDriverWait(self.driver, consts.DEFAULT_TIMEOUT_SECONDS).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="Search results."]')))
+            search_results = side_pane.find_element(By.CSS_SELECTOR, '[aria-label="Search results."]')
+            if search_results is None:
+                return False
+            search_results = search_results.find_elements(By.XPATH, "./*")            
+            if len(search_results) > 2:
+                print("MAIS DE UM CONTATO ENCONTRADO!!!!111!")
+                input()
+            return True        
+        except:
+            return None
         finally:
             self.lock.release()
